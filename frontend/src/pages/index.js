decb53d426131b85d83c730a2a48d96ac2958ef2
jigneshtalpada <jigneshtalpada636@gmail.com>
2026-01-03 14:45:57 +0530

    `Refactor authentication logic and routes`

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,12 +1,17 @@
-import { useEffect } from "react";
-import { useRouter } from "next/router";
+import Head from "next/head";
+import LoginForm from "@/components/auth/LoginForm";
+
 
 export default function Home() {
-  const router = useRouter();
-
-  useEffect(() => {
-    router.replace("/auth/login");
-  }, []);
-
-  return null;
+  return (
+    <>
+      <Head>
+        <title>SmoothSync Peoples</title>
+        <meta name="description" content="SmoothSync Peoples" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <LoginForm/>
+    </>
+  );
 }
